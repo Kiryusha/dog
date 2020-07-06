7326307b352798428f0b1a8ca227696351b63493
Kir Kostin <GiMun0@gmail.com>
2020-07-06 23:01:08 +0300

    Добавлено апи

---
+++ src/api/dog.js
@@ -0,0 +1,22 @@
+import fetcher from '@/util/fetcher'
+
+export function fetchAllBreeds () {
+  return fetcher({
+    method: 'GET',
+    url: '/api/breeds/list/all'
+  })
+}
+
+export function fetchBreed ({ breed, amount }) {
+  return fetcher({
+    method: 'GET',
+    url: `/api/breed/${breed}/images/${amount}`
+  })
+}
+
+export function fetchRandom (amount) {
+  return fetcher({
+    method: 'GET',
+    url: `/api/breeds/image/random/${amount}`
+  })
+}
