25d5552a374f80aaf6829752df004271a29b7c70
Kir Kostin <GiMun0@gmail.com>
2020-07-08 11:17:48 +0300

    Добавлена загрузка глобальных компонентов

---
+++ src/components/general/AppIcon.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  functional: true,
+
+  props: {
+    /**
+     * Имя иконки, в формате camelCase
+     */
+    name: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  },
+
+  render (h, context) {
+    const { props, data } = context
+
+    // eslint-disable-next-line
+    import(/* webpackChunkName: "[request]" */ '@/assets/icons/' + props.name + '.svg')
+
+    const useData = {
+      attrs: { 'xlink:href': `#${props.name}` }
+    }
+
+    return <svg
+      class={'app-icon'}
+      {...data}
+    >
+      <use {...useData} />
+    </svg>
+  }
+}
+</script>
